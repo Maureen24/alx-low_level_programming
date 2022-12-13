05bea2b3d51d7244f6ef7b59076cf6081a88b221
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-13 15:15:31 -0800

    putchar on integers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers 0 - 9 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
