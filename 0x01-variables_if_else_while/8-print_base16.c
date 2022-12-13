8cdcd4718aaac59004461d70844dd403877d5e3e
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-13 15:54:06 -0800

    0 - 9 and a - f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints 0 - 9 and a - f
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters = 'a';
+
+	for (int i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(letters);
+			letters++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
