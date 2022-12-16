cb71df3cb71b25bd21066322fa0fd6a10a99fd0b
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-15 17:05:47 -0800

    addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * Description: add - adds two numbers
+  *@a: parameter-1 in use
+  *@b: parameter-2 in use
+  *Return: result of addition
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
