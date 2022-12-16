fcdfe87214881c73e1bc57ee5465b5473628d5d2
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-15 16:13:47 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * Description: _abs - computes the absolute value of a number
+  * @n: parameter in use
+  * Return: Always 0 (Success)
+  */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
