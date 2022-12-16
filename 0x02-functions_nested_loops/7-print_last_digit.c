699586dd93fc7f74dbc6667b45e2cbaa3a0d8eff
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-15 16:27:40 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,12 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n >= 0)
-		return (n % 10);
+		last_digit = n % 10;
 	else
-		return ((n % 10) * -1);
+		last_digit = (n % 10) * -1;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
