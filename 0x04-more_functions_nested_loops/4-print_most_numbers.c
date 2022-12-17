700d009dc2ff236cd462dd7adce8de70241e2f6f
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-16 18:00:48 -0800

    not 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * Description: print_most_numbers - skip 2 and 4
+  * Return : 0
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar('0' + i);
+	}
+
+	_putchar('\n');
+}
