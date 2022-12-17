42e0a76b3be246d3541f05d4567b4266ac2002ed
Maureen24 <26273843+Maureen24@users.noreply.github.com>
2022-12-16 17:46:57 -0800

    product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * Description: mul - multiplies two values
+  * @a: integer1
+  * @b: integer2
+  * Return: the product
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
